b66a3f3caf49026df94b328c2c1cf43a7cd9f50e
Marcos Marques <67377101+maleixorm@users.noreply.github.com>
2024-09-24 16:38:51 -0300

    Criando alternância da seção ativa e o sticky da barra de navegação no JS

---
+++ js/script.js
@@ -0,0 +1,24 @@
+// Modifica a cor da seção ativa do site
+let sections = document.querySelectorAll('section');
+let navLinks = document.querySelectorAll('header nav a');
+
+window.onscroll = () => {
+    sections.forEach(sec => {
+        let top = window.scrollY;
+        let offset = sec.offsetTop - 150;
+        let height = sec.offsetHeight;
+        let id = sec.getAttribute('id');
+
+        if (top >= offset && top < offset + height) {
+            navLinks.forEach(links => {
+                links.classList.remove('active');
+                document.querySelector('header nav a[href*=' + id + ']').classList.add('active');
+            })
+        }
+    });
+};
+
+// Sticky da barra de navegação
+let header = document.querySelector('header');
+
+header.classList.toggle('sticky', window.scrollY > 100);
