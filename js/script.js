d03115b378572390344c71b19d0de1a4d85cded2
Marcos Marques <67377101+maleixorm@users.noreply.github.com>
2024-09-25 11:26:40 -0300

    Adicionando o scroll reveal e removendo imagens ao click

--- js/script.js
+++ js/script.js
@@ -25,9 +25,25 @@ window.onscroll = () => {
             })
         }
     });
+
+    // Sticky da barra de navegação
+    let header = document.querySelector('header');
+    header.classList.toggle('sticky', window.scrollY > 100);
+
+    // Remove o menu quando clica no link
+    menuIcon.classList.remove('bx-x');
+    navbar.classList.remove('active');
 };
 
-// Sticky da barra de navegação
-let header = document.querySelector('header');
+// Scroll Reveal
+ScrollReveal({ 
+    reset:true,
+    distance: '80px',
+    duration: 2000,
+    delay: 200 
+});
 
-header.classList.toggle('sticky', window.scrollY > 100);
+ScrollReveal().reveal('.home-content, .heading', { origin: 'top' });
+ScrollReveal().reveal('.home-img, .services-container, .portfolio-box, .contact form', { origin: 'bottom' });
+ScrollReveal().reveal('.home-content h1, .about-img', { origin: 'left' });
+ScrollReveal().reveal('.home-content p, .about-content', { origin: 'right' });
